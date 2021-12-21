c6cb887c36626ee45bd94be871e7784d1d44e184
gapaza <gapaza@tamu.edu>
2021-12-21 10:46:15 -0600

    updates

--- excel/exercises/connection/Request.py
+++ excel/exercises/connection/Request.py
@@ -1,23 +1,82 @@
 import requests
+import pickle
+from pathlib import Path
+import os
 
 version = 'dev'
+
 
 class RequestClient:
     def __init__(self):
         self.url = None
-        self.dev_url = 'http://localhost:8080/api/'
+        self.dev_url = 'http://localhost:8000/api/'
         self.prod_url = 'https://dev2.selva-research.com/api/'
         if version == 'prod':
             self.url = self.prod_url
         else:
             self.url = self.dev_url
 
+        # --> Session Data
+        root_file_path = os.path.dirname(os.path.realpath(__file__))
+        self.session_file_path = os.path.join(root_file_path, 'profile', 'session.pickle')
+        # self.session = self.get_or_create_session()
+        self.session = requests.session()
+
+    def __del__(self):
+        self.save_session()
+
+    """
+       _____               _             
+      / ____|             (_)            
+     | (___   ___  ___ ___ _  ___  _ __  
+      \___ \ / _ \/ __/ __| |/ _ \| '_ \ 
+      ____) |  __/\__ \__ \ | (_) | | | |
+     |_____/ \___||___/___/_|\___/|_| |_|
+    """
+
+
+    def get_or_create_session(self):
+        # --> 1. Check to see if session already exists
+        session_file = Path(self.session_file_path)
+        if not session_file.is_file():
+            return requests.session()
+        else:
+            session_file_obj = session_file.open(mode='rb')
+            return pickle.load(session_file_obj)
+
+    def delete_session(self):
+        session_file = Path(self.session_file_path)
+        if session_file.is_file():
+            print('--> DELETING SESSION FILE')
+            os.remove(self.session_file_path)
+
+    def save_session(self):
+        session_file = Path(self.session_file_path)
+        session_file_obj = session_file.open(mode='wb')
+        pickle.dump(self.session, session_file_obj)
+
+    """
+      ______                     _       
+     |  ____|                   | |      
+     | |__  __  _____  ___ _   _| |_ ___ 
+     |  __| \ \/ / _ \/ __| | | | __/ _ \
+     | |____ >  <  __/ (__| |_| | ||  __/
+     |______/_/\_\___|\___|\__,_|\__\___|                              
+    """
+
     def execute(self, extension, reqData):
         request_url = self.url + extension
         result = None
         try:
-            result = requests.post(request_url, data=reqData)
+            result = self.session.post(request_url, data=reqData)
         except Exception as e:
             print('--> CONNECTION ERROR', e)
             pass
-        return result
+        print('--> REQUEST RESULT:', type(result), result)
+        return result.json()
+
+
+
+
+
+
--- excel/exercises/main.py
+++ excel/exercises/main.py
@@ -1,38 +1,96 @@
 import xlwings as xw
 
-
-def connect():
-    wb = xw.Book.caller()
-    sheet = wb.sheets[0]
-    if sheet["A1"].value == "Hello xlwings!":
-        sheet["A1"].value = "Bye xlwings!"
-    else:
-        sheet["A1"].value = "Hello xlwings!"
+from connection.Request import RequestClient
 
 
-@xw.func
-def connect_to_daphne(username, password):
-    print('--> USERNAME:', username)
-    print('--> PASSWORD:', password)
-
-    # --> 1. Connect to daphne here
-
-    # --> 2. Return login results to VBA
-    return_status = ['Success', 'Invalid Credentials']
-    return return_status[1]
-
-
-
-@xw.func
-def hello(name):
-    return f"Hello {name}!"
-
-
-@xw.func
-def double_sum(x, y):
-    """Returns twice the sum of the two arguments"""
-    return 8 * (x + y)
+"""
+        ___  ___      _       
+        |  \/  |     (_)      
+        | .  . | __ _ _ _ __  
+        | |\/| |/ _` | | '_ \ 
+        | |  | | (_| | | | | |
+        \_|  |_/\__,_|_|_| |_|                  
+"""
 
 
 if __name__ == "__main__":
     xw.Book("tutorial.xlsm").set_mock_caller()
+    xw.serve()
+
+
+"""
+         _   _____________   
+        | | | |  _  \  ___|  
+        | | | | | | | |_ ___ 
+        | | | | | | |  _/ __|
+        | |_| | |/ /| | \__ \
+         \___/|___/ \_| |___/        
+"""
+
+
+@xw.func
+def request_chat_history():
+    client = RequestClient()
+    # result = client.execute('ca/dialogue/command_history', {})
+    # if 'response' in result:
+    #     return result['response']
+    return ""
+
+
+@xw.func
+def request_ask_question(question, workbook, worksheet, cell):
+    print('--> QUESTION:', question, workbook, worksheet, cell)
+    client = RequestClient()
+    result = client.execute('ca/dialogue/command', {'command': question, 'workbook': workbook, 'worksheet': worksheet, 'cell': cell})
+    if 'response' in result:
+        return result['response']
+    return "no response"
+
+
+@xw.func
+def request_new_user(username, email, password_1, password_2):
+    client = RequestClient()
+    result = client.execute('auth/register', {'username': username, 'email': email, 'password1': password_1, 'password2': password_2})
+    print('--> NEW USER RESULT', result)
+    if 'status' in result:
+        if 'registration_error' in result:
+            return result['registration_error']
+        return result['status']
+    return 'local error'
+
+
+@xw.func
+def request_login(username, password):
+    client = RequestClient()
+    result = client.execute('auth/login', {'username': username, 'password': password})
+    print('--> LOGIN RESULT', result)
+    if 'status' in result:
+        return result['status']
+    return 'local error'
+
+
+@xw.func
+def request_logout():
+    client = RequestClient()
+    result = client.execute('auth/logout', {})
+    print('--> LOGOUT RESULT', result)
+    if 'message' in result:
+        return result['message']
+    return 'local error'
+
+
+@xw.func
+def request_new_session():
+    client = RequestClient()
+    client.delete_session()
+    return 'session removed'
+
+
+@xw.func
+def request_event(cell_row, cell_col, cell_value):
+    print('--> SENDING EVENT:', cell_row, cell_col, cell_value)
+    client = RequestClient()
+    result = client.execute('ca/dialogue/event', {'row': cell_row, 'col': cell_col, 'value': cell_value})
+    if 'response' in result:
+        return result['response']
+    return "no response"
